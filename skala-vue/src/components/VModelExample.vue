9f495d4653933618314b8d1e879a4c355e384f38
kamillcream <kamillcream1@gmail.com>
2026-08-25 14:01:24 +0900

    feat: 실습 구현

---
+++ skala-vue/src/components/VModelExample.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>과제 3: 양방향 바인딩 및 한글 처리 (:value, @input)</h2>
+    <input type="text" v-model="text1" placeholder="여기에 입력하세요" />
+    <p>
+    입력된 값: <strong>{{ text1 }}</strong>
+    </p>
+    <input type="text" :value="text2" @input="(e) => (text2 = e.target.value)" placeholder="원리 파악용 입력창" />
+    <p>
+    입력된 값: <strong>{{ text2 }}</strong>
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const text1 = ref('') 
+const text2 = ref('') 
+
+</script>
+
+<style>
+
+</style>

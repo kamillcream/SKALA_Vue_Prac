ba6913629730410791f8bf95ffcda0a69d3d85c4
kamillcream <kamillcream1@gmail.com>
2026-08-26 11:33:20 +0900

    emit, prop 코드 챌린지 구현

---
+++ skala-vue/src/components/ChildComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>이름 : {{ props.name }} </p>
+        <input v-model="tel" type="text" placeholder="전화번호를 입력하세요">
+        <button @click="onConfirm()">제출</button>
+    </div>
+</template>
+
+<script setup>
+import { ref, defineEmits, defineProps } from 'vue';
+const tel = ref('');
+const emit = defineEmits(['confirm']);
+const props = defineProps(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+    }
+)
+
+const onConfirm = () => {
+    emit('confirm', tel.value);
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ skala-vue/src/components/ParentComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div> 
+        <input v-model="nameInput" placeholder="이름을 입력하세요">
+        <ChildComponent :name="nameInput" @confirm="handleConfirm"/>
+        <p v-if="phone.length != 0">{{ phone }}</p>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import ChildComponent from './ChildComponent.vue';
+
+const nameInput = ref('');
+const phone = ref('');
+
+const handleConfirm = (tel) => {
+    console.log("전화번호: ", tel)
+    phone.value = tel;
+}
+
+</script>
+
+<style>
+
+</style>
